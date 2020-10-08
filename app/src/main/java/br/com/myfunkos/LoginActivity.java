e1fc3fb19c19b160620c27634318071211908c02
Edvardo Guerra <edvardo@uol.com.br>
2020-10-08 01:13:25 -0300

    Inserida tela de login usando autenticação pelo Firebase

---
+++ app/src/main/java/br/com/myfunkos/LoginActivity.java
@@ -0,0 +1,141 @@
+package br.com.myfunkos;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import br.com.myfunkos.activity.MainActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public FirebaseAuth fAuth;
+    EditText emailEditText;
+    EditText senhaEditText;
+    Button entrarBotao;
+    Button criarBotao;
+    Button esqueciBotao;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
+        inicializarElementos();
+
+    } //fim de onCreate
+
+    private void inicializarElementos() {
+        emailEditText = findViewById(R.id.login_email);
+        senhaEditText = findViewById(R.id.login_senha);
+        configurarBotaoEntrar();
+        configurarBotaoCriar();
+        configurarBotaoRedefinir();
+    }
+
+    private void configurarBotaoRedefinir() {
+        esqueciBotao = findViewById(R.id.login_botao_esqueci_senha);
+        esqueciBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redefinirSenha();
+            }
+        });
+    }
+
+    private void configurarBotaoCriar() {
+        criarBotao = findViewById(R.id.login_botao_criar);
+        criarBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                criarUser();
+            }
+        });
+    }
+
+    private void configurarBotaoEntrar() {
+        entrarBotao = findViewById(R.id.login_botao_login);
+        entrarBotao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checarLoginSenha();
+            }
+        });
+    }
+
+    private void redefinirSenha() {
+        String email = emailEditText.getText().toString();
+        fAuth.sendPasswordResetEmail(email);
+    }
+
+    private void checarLoginSenha() {
+
+        String email = emailEditText.getText().toString();
+        String senha = senhaEditText.getText().toString();
+        email = "edvardo@uol.com.br";
+        senha = "smdpdm2020";
+
+        fAuth.signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            if (!user.isEmailVerified()) { //checa se email foi verificado
+                                Toast.makeText(getApplicationContext(), "Verificar email de confirmação",
+                                        Toast.LENGTH_LONG).show();
+                                user.sendEmailVerification();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Usuário logado...",
+                                        Toast.LENGTH_LONG).show();
+                                vaiParaMain();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Deu errado",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void vaiParaMain() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void criarUser() {
+        String email = emailEditText.getText().toString();
+        String senha = senhaEditText.getText().toString();
+
+        fAuth.createUserWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Usuário criado
+                            Toast.makeText(getApplicationContext(), "Usuário Criado", Toast.LENGTH_LONG).show();
+                        } else {
+                            //Não criado
+                            Log.v("myfunko", "Exceção " + task.getException());
+                        }
+                    }
+                });
+
+
+    }
+
+}
--- app/src/main/java/br/com/myfunkos/activity/NovoItemActivity.java
+++ app/src/main/java/br/com/myfunkos/activity/NovoItemActivity.java
@@ -1,13 +1,27 @@
 package br.com.myfunkos.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 import br.com.myfunkos.R;
@@ -16,8 +30,11 @@ import br.com.myfunkos.model.Item;
 public class NovoItemActivity extends AppCompatActivity {
 
     public static final String TITULO_APPBAR = "Novo Item";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_TAKE_PHOTO = 1234;
 
     private Item item;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,81 @@ public class NovoItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_novo_item);
         setTitle(TITULO_APPBAR);
 
+        configuraBotaoNovoItem();
+
+        Button tirarFoto = findViewById(R.id.botao_tirar_foto);
+        tirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                obterPermissoes();
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+
+            }
+        });
+    }
+
+    private void obterPermissoes() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else
+            dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile("PHOTOAPP", ".jpg", storageDir);
+                mCurrentPhotoPath = "file:" + photoFile.getAbsolutePath();
+            } catch (IOException ex) {
+                Toast.makeText(getApplicationContext(), "Erro ao tirar a foto", Toast.LENGTH_SHORT).show();
+            }
+
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                ImageView imagem = (ImageView) findViewById(R.id.detalhe_item_imagem);
+                Bitmap bm1 = BitmapFactory.decodeStream(getContentResolver().openInputStream(Uri.parse(mCurrentPhotoPath)));
+                imagem.setImageBitmap(bm1);
+            } catch (FileNotFoundException fnex) {
+                Toast.makeText(getApplicationContext(), "Foto não encontrada!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    dispatchTakePictureIntent();
+                } else {
+                    Toast.makeText(this, "Não vai funcionar!!!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void configuraBotaoNovoItem() {
         Button salvarItem = findViewById(R.id.botao_salvar);
         salvarItem.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +127,7 @@ public class NovoItemActivity extends AppCompatActivity {
             }
         });
     }
+
 
     private Item obterNovoItem() {
 
